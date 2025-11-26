1721898ea7b0eea3f9202ace27eb847e705444cc
AreebaFatima-123 <areebarafaqat456@gmail.com>
2025-11-26 09:25:17 +0000

    parentGuidanceScreen

--- TheoryAssignment3/App.js
+++ TheoryAssignment3/App.js
@@ -156,3 +156,18 @@ const ProfileScreen = ({ route }) => {
     </View>
   );
 };
+/* --------------------------------------------------
+   5. PARENTS GUIDE SCREEN
+-------------------------------------------------- */
+const ParentsGuideScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Parents Guide</Text>
+
+      <Text style={styles.text}>✔ Vaccines protect your child.</Text>
+      <Text style={styles.text}>✔ Keep vaccination card safe.</Text>
+      <Text style={styles.text}>✔ Mild fever after vaccine is normal.</Text>
+      <Text style={styles.text}>✔ Follow national immunization schedule.</Text>
+    </ScrollView>
+  );
+};
