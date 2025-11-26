c4154d4b12bdf0aad4f85966051563c4d9cc49b7
nayyabkhadija11 <nayyabkhadija5@gmail.com>
2025-11-26 17:34:21 +0500

    VACCINATION SCHEDULE SCREEN

--- TheoryAssignment3/App.js
+++ TheoryAssignment3/App.js
@@ -171,3 +171,26 @@ const ParentsGuideScreen = () => {
     </ScrollView>
   );
 };
+/* --------------------------------------------------
+   6. VACCINATION SCHEDULE SCREEN
+-------------------------------------------------- */
+const VaccinationScheduleScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Vaccination Schedule</Text>
+
+      <View style={styles.card}>
+        <Text style={styles.smallHeading}>Birth</Text>
+        <Text>• BCG</Text>
+        <Text>• OPV 0</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.smallHeading}>6 Weeks</Text>
+        <Text>• OPV 1</Text>
+        <Text>• Penta 1</Text>
+        <Text>• PCV 1</Text>
+      </View>
+    </View>
+  );
+};
