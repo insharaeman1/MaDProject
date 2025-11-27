eec164166b163b6808ad9fb4d163d6e3a97a39f0
insharaeman1 <insharaeman5@gmail.com>
2025-11-26 17:43:49 -0800

    main

--- TheoryAssignment3/App.js
+++ TheoryAssignment3/App.js
@@ -229,3 +229,69 @@ export default function App() {
     </NavigationContainer>
   );
 }
+/* --------------------------------------------------
+   STYLES
+-------------------------------------------------- */
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 25,
+  },
+  heading: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 15,
+    color: "#0A6EBD",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#bbb",
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 10,
+  },
+  btn: {
+    backgroundColor: "#0A6EBD",
+    padding: 15,
+    width: "80%",
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    elevation: 3,
+  },
+  info: {
+    fontSize: 18,
+    marginVertical: 5,
+  },
+  link: {
+    marginTop: 15,
+    textAlign: "center",
+    color: "blue",
+  },
+});
